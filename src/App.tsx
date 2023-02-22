6fd6fa7cfd7a687a2282b59441e966732757aa0a
unknown <pankajj@wooshelf.com>
2023-02-22 12:02:29 -0800

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import Footer from "./components//HomePage/Footer";
-import Header from "./components//HomePage/Header";
+import Header from "./components/HomePage/Header";
 // import Header from "../Components/Header";
 import img1 from "./assets/howitworks1.png"
 import img2 from "./assets/howitworks2.png"
@@ -10,85 +10,79 @@ import BM from "./assets/BM.png";
 // import Image from "./assets/startdesigning.png";
 import Image1 from "./assets/CardImg1.png";
 import Image2 from "./assets/CardImg2.png";
+import { redis } from "googleapis/build/src/apis/redis";
 // import Image from "../Assets/MainBaneer.png";
 // import Image2 from "../Assets/IMGMOB.png";
-// import Image from "../Assets/editingCompany.png";
+// import Image from "../Assets/editingCompany.png"
+import "./styles/Newdesin.css";
+
 
 
 const App = () => {
   return <>
     <Header />
-    <div className="bannerSec">
-      <div className="centerdiv">
-        <h3>
-          An Advance Media <br /> Editing Platform
-        </h3>
-        <h1>
-          Now edit images & Videos within a Click , An AI Embeded <br /> Media
-          Editing Platform
-        </h1>
-        <div className="centredivbuitton">
-          {/* <NavLink to="/edit"> */}
-            <button>Start a WhiteBoard</button>
-          {/* </NavLink> */}
-        </div>
-        <p>Free Forever No credit Card Required </p>
-      </div>
+    <div style={{textAlign:"center"}}>
+      <div  style={{ textAlign:"center",marginTop:"35px",fontSize:"55px", marginBottom:"10px"}} >
+          <strong> An Advance Media <br /> Editing Platform </strong>
+        <p style={{fontSize:"19px",marginTop:"35px"}}>
+          Now edit images & Videos within a Click , An AI Embeded <br /> 
+          MediaEditing Platform 
+        </p>
+        <button style={{backgroundColor:"#449ce7", borderRadius:"10px", fontSize:"25px"}}>Start a WhiteBoard</button>
+        <p style={{fontSize:"14px",marginTop:"9px"}}>Free Forever No credit Card Required </p>
     </div>
+    
     <div className="BannerSecMobileImage">
-        <img src={BM} alt="" style={{ width: "100%" }} />
+        <img src={BM} alt="" style={{ width: "50%" }} />
       </div>
-      <div id="howitworks" style={{ width: "100%" }}>
+      {/* <div id="howitworks" style={{ width: "100%" }}>
       <div className="howitworks-left-inner" style={{ marginBottom: "30px" }}>
+      </div> */}
         {/* <h1>How to edit your Image & videos in a snap</h1> */}
-      </div>
-      <div className="howitworkssec">
-        <div className="howitworkssec-righsec">
-          <div className="howitworks-right-inner">
+      <div style={{ display:"flex", textAlign:"center", flexDirection:"row",fontSize:"13px",marginTop:"250px"}}>
+          <div style={{marginLeft:"90px"}}>
             <img src={img1} alt="" style={{ width: "20%", margin: "20px" }} />
             <h1>SignUp</h1>
             <p>
-            For registration, fill in the required information, create an account, and set a password.
+            For registration, fill in the required information,<br /> create an account, and set a password.
             </p>
           </div>
-          <div className="howitworks-right-inner">
+          <div style={{marginLeft:"90px"}}>
             <img src={img2} alt="" style={{ width: "20%", margin: "20px" }} />
             <h1>SignIn</h1>
             <p>
-            To access your account, enter your user id and password. And click <b>SignIn.</b> 
+            To access your account, enter your user id and <br /> password. And click <b>SignIn.</b> 
 
             </p>
           </div>
-          <div className="howitworks-right-inner">
+          <div style={{marginLeft:"90px"}}>
             <img src={img3} alt="" style={{ width: "20%", margin: "20px" }} />
             <h1>Edit</h1>
             <p>
-            Upload an image or video from your device and edit it.
+            Upload an image or video from your device <br /> and edit it.
             </p>
           </div>
-        </div>
+        
       </div>
-    </div>
-    <div className="mainsection">
-      <div className="uploadvideo-leftsec">
-        <img src={Image} alt="" style={{ width: "100%" }} />
+    
+    <div >
+      <div style={{textAlign:"left"}}>
+        <img src={Image} alt="" style={{ width: "40%",marginLeft:"53px",marginTop:"90px" }} />
       </div>
-      <div className="uploadvideo-rightsec">
-        <div className="uploadvideo-innersection">
-          <h1>Online Image editor for everyone</h1>
-          <p>
+      <div>
+        <div style={{ textAlign:"left",marginTop:"-282px",marginLeft:"450px"}}>
+          <h1>Online Image editor for <br /> everyone</h1>
+          <p style={{fontSize:"10px",marginTop:"-70px"}} >
             Freeditor image editor lets you edit photos in just a few clicks. It
             covers various free online image editing tools,
           </p>
-          {/* <button  onClick={() => {
-              props.setSelectedTool('image-editor')
-          }}>Upload your Image</button> */}
+          <button>Upload your Image</button>
         </div>
       </div>
     </div>
-    <div className="mainsection" style={{marginTop: "0%"}}>
-            <div className="uploadvideo-rightsec">
-                <div className="uploadvideo-innersection">
+    <div style={{marginTop: "10px",marginLeft:"53px" }}>
+            <div style={{textAlign:"left"}}>
+                <div style={{ width: "40%",marginLeft:"53px",marginTop:"90px" }}>
                     <h1>Easy video editing for everyone</h1>
                     <p>
                         Our video editor comes with all the features required to create
@@ -171,6 +165,7 @@ const App = () => {
         </div>
         <div className="faqinner">
         <div className="faq-col faqwidth">What's the difference between Freeditor and Photoshop?</div>
+        </div>
         </div>
 
     {/* <Footer /> */}
--- src/components/HomePage/Header.jsx
+++ src/components/HomePage/Header.jsx
@@ -1,65 +1,155 @@
 import React, {useEffect, useRef} from "react";
+import LogoImg from "../../assets/LOGOIMG.png";
+import {Link, NavLink} from "react-router-dom";
+import {Avatar, Divider} from "@mui/material";
+import avtar from "../../assets/avatar.jpg";
+import {deepOrange} from "@mui/material/colors";
+import {useDispatch, useSelector} from 'react-redux'
+import { setCardStatus} from "../../redux/slices/TextSlice";
+import {logoutUserAuthAction} from "../../redux/actions/authReduxActions";
+import {Dropdown, Menu} from "antd";
 
-const Header = ({handleshowlogin, handleMethod}) => {
-    const isLoggedIn = false
-
-    const box = useRef(null);
-    useOutsideAlerter(box);
-
-    // const showExtra = useSelector(state => state.TextSlice.card_status);
-    // const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-    // const dispatch = useDispatch();
-
-    const handleClickLogin = () => {
-        localStorage.setItem("header", 1);
-        handleshowlogin();
-        handleMethod("Login");
-    };
-
-    const handleLoginMethodSignup = () => {
-        localStorage.setItem("header", 1);
-        handleshowlogin();
-        handleMethod("Signup");
-    };
-
-
-    function logout() {
-        // dispatch(logoutUserAuthAction())
-        // localStorage.removeItem("auth_token");
-        // localStorage.removeItem("isLoggedIn");
-
-
-        // dispatch(setCardStatus(false))
-
-        // navigate("/");
-    }
-
-
-    function showExtraOptions() {
-        // dispatch(setCardStatus(!showExtra))
-        // console.log(showExtra);
-    }
-
-    const onHandleMenu = (event) => {
-        switch (event.key) {
-            case "logout":
-                logout()
-                break;
-            case "my-design":
-                handleMethod('my-all-designs')
-                break;
-        }
-        console.log("===============", event)
-    }
-
-
+const Header = ( ) => {
 
     return (
         <div className="header">
+            <NavLink to="/">
+                <div className="headerleft">
+
+                    <img src={LogoImg} alt="" className="headerlogo"/>
+
+                </div>
+            </NavLink>
             <div className="header-text" style={{
-                paddingLeft: 171 }}>This is beta version
+                paddingLeft: !isLoggedIn ? 171 : 0
+            }}>This is beta version
             </div>
-            <div className="headerright">
+            {isLoggedIn ? (
+                <>
+                    <div className="profileIcon">
+                        <Dropdown overlayStyle={{
+                            width: '150px'
+                        }} overlay={userMenu} trigger={'click'} placement="bottomRight">
+                            <button
+                                ref={box}
+                                onClick={showExtraOptions}
+                                style={{border: "none", background: "none"}}
+                            >
+                                <Avatar
+                                    src={avtar}
+                                    sx={{
+                                        width: 50,
+                                        height: 50,
+                                        bgcolor: deepOrange[500],
+                                    }}
+                                    alt="photoURL"
+                                />
+                            </button>
+                        </Dropdown>
+                        {/*<div*/}
+                        {/*  style={{*/}
+                        {/*    width: "200px",*/}
+                        {/*    display: `${showExtra == false ? "none" : "block"}`,*/}
+                        {/*    position: "fixed",*/}
+                        {/*  }}*/}
+                        {/*>*/}
+                        {/*  <Card style={{textAlign:'center'}}>*/}
+                        {/*    <Link to={'/'}>*/}
+                        {/*    <button*/}
+                        {/*      onClick={() => {*/}
+                        {/*        // navigate("/")*/}
+                        {/*        dispatch(setCardStatus(false))*/}
+                        {/*      }}*/}
+                        {/*      style={{*/}
+                        {/*        border: "none",*/}
+                        {/*        background: "none",*/}
+                        {/*        margin: "10%",*/}
+                        {/*        color:"#292d31"*/}
+                        {/*      }}*/}
+                        {/*    >*/}
+                        {/*      <div>Home</div>*/}
+                        {/*    </button>*/}
+                        {/*    </Link>*/}
+
+                        {/*    <hr style={{ margin: "1%" }} />*/}
+                        {/*    <Link to={'/image-editor'}>*/}
+                        {/*    <button*/}
+                        {/*      onClick={() => {*/}
+                        {/*        dispatch(setCardStatus(false))*/}
+                        {/*      }}*/}
+                        {/*      style={{*/}
+                        {/*        border: "none",*/}
+                        {/*        background: "none",*/}
+                        {/*        margin: "10%",*/}
+                        {/*        color:"#292d31"*/}
+                        {/*      }}*/}
+                        {/*    >*/}
+                        {/*      <div>New Editor</div>*/}
+                        {/*    </button>*/}
+                        {/*    </Link>*/}
+                        {/*    <hr style={{ margin: "1%" }} />*/}
+
+                        {/*    <Link to={'/medialibrary'}>*/}
+                        {/*    <button*/}
+                        {/*      onClick={() => {*/}
+                        {/*        dispatch(setCardStatus(false))*/}
+                        {/*      }}*/}
+                        {/*      style={{*/}
+                        {/*        border: "none",*/}
+                        {/*        background: "none",*/}
+                        {/*        margin: "10%",*/}
+                        {/*        color:"#292d31"*/}
+                        {/*      }}*/}
+                        {/*    >*/}
+                        {/*      <div>Dashboard</div>*/}
+                        {/*    </button>*/}
+                        {/*    </Link>*/}
+
+                        {/*    <hr style={{ margin: "1%" }} />*/}
+                        {/*    <Link to={'/my-all-designs'}>*/}
+                        {/*    <button*/}
+                        {/*      onClick={() => {*/}
+                        {/*        dispatch(setCardStatus(false))*/}
+                        {/*      }}*/}
+                        {/*      style={{*/}
+                        {/*        border: "none",*/}
+                        {/*        background: "none",*/}
+                        {/*        margin: "10%",*/}
+                        {/*        color:"#292d31"*/}
+                        {/*      }}*/}
+                        {/*    >*/}
+                        {/*      <div>My Design</div>*/}
+                        {/*    </button>*/}
+                        {/*    </Link>*/}
+                        {/*    <hr style={{ margin: "1%" }} />*/}
+
+                        {/*    <button*/}
+                        {/*      onClick={logout}*/}
+                        {/*      style={{ border: "none", background: "none", margin: "10%" ,color:"#292d31" }}*/}
+                        {/*    >*/}
+                        {/*      <div>Logout</div>*/}
+                        {/*    </button>*/}
+                        {/*  </Card>*/}
+                        {/*</div>*/}
+                    </div>
+
+                    {/* <Popover
+            open={true}
+            // anchorEl={open}
+            // onClose={handleClose}
+            sx={{
+              p: 0,
+              mt: 1.5,
+              ml: 0.75,
+              "& .MuiMenuItem-root": {
+                typography: "body2",
+                borderRadius: 0.75,
+              },
+            }}
+          ></Popover> */}
+                </>
+            ) : (
+                <div className="headerright">
                     <button className="login-btn" onClick={handleClickLogin}>
                         Login
                     </button>
@@ -67,6 +157,7 @@ const Header = ({handleshowlogin, handleMethod}) => {
                         Sign Up
                     </button>
                 </div>
+            )}
         </div>
     );
 };
@@ -75,13 +166,13 @@ export default Header;
 
 
 function useOutsideAlerter(ref) {
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     // const showExtra = useSelector(state => state.TextSlice.card_status);
     useEffect(() => {
         // Function for click event
         function handleOutsideClick(event) {
             if (ref.current && !ref.current.contains(event.target)) {
-                // dispatch(setCardStatus(false))
+                dispatch(setCardStatus(false))
             }
         }
 
